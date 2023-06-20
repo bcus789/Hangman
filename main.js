34b50122217a89f1525e652e484652a7e9297748
Bonello <Geno.Bonello@treasurer.ca.gov>
2023-06-20 15:27:57 -0700

    have basic game running still need to update scores

--- main.js
+++ main.js
@@ -3,43 +3,61 @@ const guessBtn = document.getElementById("submit-letter")
 const wordDiv = document.getElementById("word-div")
 const submitLetter = document.getElementById("submit-letter")
 const letterInput = document.getElementById("letter-input")
-let wordBank = ["penny"]
+const numTries = document.getElementById("tries")
+let tries = ""
+let wordBank = ["paper", "typwriter", "annabolic"]
 let chosenWord = ""
 let hiddenWord = ""
+let chosenWordArr = []
+let hiddenWordArr = []
 
 startBtn.addEventListener("click", startGame)
 
-function startGame(){
+function startGame() {
+    tries = 6
+    numTries.innerText = `Tries: ${tries}`
     chosenWord = pickWord()
-    hiddenWord = hideText(chosenWord)
-    wordDiv.innerText = hiddenWord
+    chosenWordArr = Array.from(chosenWord)
+    hideText(chosenWord)
+    hiddenWordArr = Array.from(hiddenWord)
+    renderHidden(hiddenWordArr)
 }
 
-function pickWord(){
-    for (let i = 0; i < wordBank.length; i ++){
+function pickWord() {
+    for (let i = 0; i < wordBank.length; i++) {
         const random = Math.floor(Math.random() * wordBank.length)
         let word = wordBank[random]
         return word
     }
 }
 
-function hideText(word){
-    return "-".repeat(word.length)
-}
-
-submitLetter.addEventListener("click", function(){
-    checkLetter(letterInput.value)
-})
-
-function checkLetter(letter){
-    for (let i = 0; i < chosenWord.length; i++){
-        if (chosenWord.charAt(i) === letter){
-            replaceBlank(i, letter)
-        }
+function hideText(word) {
+    for (i = 0; i < word.length; i++) {
+        hiddenWord = "-".repeat(word.length)
     }
 }
 
-function replaceBlank(index, letter){
-   let newString = hiddenWord.replace(/-/g, letter)
-   console.log(newString)
+function renderHidden(word) {
+    wordDiv.innerText = ""
+    for (i = 0; i < word.length; i++) {
+        wordDiv.innerText += hiddenWordArr[i]
+    }
+}
+
+submitLetter.addEventListener("click", compare)
+
+function compare() {
+    for (i = 0; i < chosenWordArr.length; i++) {
+        let letter = letterInput.value
+        if (chosenWordArr[i] === letter) {
+            hiddenWordArr[i] = letter
+        } 
+    }
+    updateTriesAndLosses()
+    renderHidden(hiddenWordArr)
+}
+
+function updateTriesAndLosses(){
+    tries --
+    numTries.innerText = `Tries: ${tries}`
 }
