a79fb683eb93419bd53b6ef5848cb6cf91fdca32
Bonello <geno.bonello@gmail.com>
2024-05-17 14:32:42 -0700

    restructing a basic hangman game just to keep up with skills and practice optomization

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-const startBtn = document.getElementById("start-button")
-const guessBtn = document.getElementById("submit-letter")
-const wordDiv = document.getElementById("word-div")
 const gameDiv = document.getElementById("game-div")
-const submitLetter = document.getElementById("submit-letter")
-const letterInput = document.getElementById("letter-input")
-const numTries = document.getElementById("tries")
-const lettersTried = document.getElementById("letter-tried")
+const startBtn = document.getElementById("start-button")
+const guessBtn = document.getElementById("guess-button")
+const wordDiv = document.getElementById("word-div")
+const inputField = document.getElementById("input-field")
+const numTries = document.getElementById("tries-left")
+const lettersTried = document.getElementById("letters-tried")
 const wordBank = ['paper','potato', 'mobile', 'lego', 'jacket', 'apple', 'cat', 'throne', 'street', "jungle"]
+
+let tries
+let chosenWordArr = []
+let hiddenWordArr = []
+
+const game = {
+    start: function() {
+        tries = 10
+        numTries.innerText = tries
+        chosenWordArr = Array.from(game.pickWord(wordBank))
+        hiddenWordArr = chosenWordArr.map(() => "-")
+        
+    },
+    pickWord: function(words) {
+        randomNum = Math.floor(Math.random() * words.length)
+        return words[randomNum]
+    }
+}
+
+startBtn.addEventListener("click", game.start)
 
