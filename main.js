61581ac07d024ae47a0bb100eef65ad8723bde2c
Bonello <geno.bonello@gmail.com>
2024-05-20 13:23:17 -0700

    added photos to gow ith my hangman game

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ const game = {
         gameDiv.style.display = "inline"
         startBtn.innerText = "Restart"
         tries = 10
+        img.src = "/hangman_images/hang_0.png"
         chosenWordArr = []
         hiddenWordArr = []
         chosenWordArr = Array.from(game.pickWord())
@@ -34,12 +35,39 @@ const game = {
             wordDiv.innerText += hiddenWordArr[i]
         }
         numTries.innerHTML = `Tries Left ${tries}`
+        console.log(tries)
 
         switch (tries) {
+            case 9:
+            img.src = "/hangman_images/hang_1.png";
+            break;
+            case 8:
+            img.src = "/hangman_images/hang_2.png";
+            break;
+            case 7:
+            img.src = "/hangman_images/hang_3.png";
+            break;
+            case 6:
+            img.src = "/hangman_images/hang_4.png";
+            break;
+            case 5:
+            img.src = "/hangman_images/hang_5.png";
+            break;
+            case 4:
+            img.src = "/hangman_images/hang_6.png";
+            break;
+            case 3:
+            img.src = "/hangman_images/hang_7.png";
+            break;
+            case 2:
+            img.src = "/hangman_images/hang_8.png";
+            break;
+            case 1:
+            img.src = "/hangman_images/hang_9.png";
+            break;
             case 0:
-            tries = 9;
-            img.setAttribute( "src", "/hangman_images/hang_2.png");
-
+            img.src = "/hangman_images/hang_10.png";
+            break;
         }
     },
 }
@@ -54,12 +82,11 @@ const guessedLetter = {
                 hiddenWordArr[i] = letter
             }
         }
-        game.renderScreen()
 
-            if (!chosenWordArr.includes(letter)){
-                tries --
-                game.renderScreen()
-            }
+        if (!chosenWordArr.includes(letter)){
+            tries --
+        }
+        game.renderScreen()
     }
 }
 
