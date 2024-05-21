dace87e2dc26518635c82d31a0952a23d0a77534
Bonello <geno.bonello@gmail.com>
2024-05-20 20:00:09 -0700

    needed to add some support for image displayed on reset

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const game = {
     start: function () {
         img.src = "/hangman_images/hang_0.png"
         gameDiv.style.display = "inline"
-        startBtn.innerText = "Restart"
+        startBtn.innerText = "New Word"
         lettersTried.innerText = ""
         guessBtn.disabled = false
         inputField.disabled = false
@@ -62,6 +62,8 @@ const game = {
         numWins.innerHTML = localStorage.getItem('wins')
         numLosses.innerHTML = localStorage.getItem('losses')
         numTries.innerHTML = localStorage.getItem('tries')
+        img.src = "hangman_images/hang_0.png"
+        game.renderScreen()
     },
     renderScreen: function () {
         inputField.value = null
@@ -70,37 +72,38 @@ const game = {
             wordDiv.innerText += hiddenWordArr[i]
         }
         if (!game.checkWin()) {
+            tries = localStorage.getItem("tries")
             numTries.innerHTML = tries
 
             switch (tries) {
-                case 9:
+                case "9":
                     img.src = "hangman_images/hang_1.png"
                     break;
-                case 8:
+                case "8":
                     img.src = "hangman_images/hang_2.png"
                     break;
-                case 7:
+                case "7":
                     img.src = "hangman_images/hang_3.png"
                     break;
-                case 6:
+                case "6":
                     img.src = "hangman_images/hang_4.png"
                     break;
-                case 5:
+                case "5":
                     img.src = "hangman_images/hang_5.png"
                     break;
-                case 4:
+                case "4":
                     img.src = "hangman_images/hang_6.png"
                     break;
-                case 3:
+                case "3":
                     img.src = "hangman_images/hang_7.png"
                     break;
-                case 2:
+                case "2":
                     img.src = "hangman_images/hang_8.png"
                     break;
-                case 1:
+                case "1":
                     img.src = "hangman_images/hang_9.png"
                     break;
-                case 0:
+                case "0":
                     img.src = "hangman_images/hang_10.png"
                     guessBtn.disabled = true
                     inputField.disabled = true
